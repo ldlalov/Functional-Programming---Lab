3cec6d2dedc04bfc7d596d1fd7cb53d2ec322add
Lyubomir Lalov <ldlalov@hotmail.com>
2022-08-21 16:37:22 +0300

    Add project files.

---
+++ 01. Sort Even Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(x => int.Parse(x)).Where(x=>x%2 == 0).OrderBy(x=>x).ToArray();
+            Console.WriteLine(String.Join(", ",numbers));
+        }
+    }
+}
